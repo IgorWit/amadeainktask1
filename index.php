ce5057b69c0539fc32c8151f7f7e2bc33bb5970b
IgorWit <vitruk.igor1@gmail.com>
2017-04-06 10:22:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AmadeaInk</title>
+	<link rel="stylesheet" type="text/css" href="fonts/glyphicons-halflings-regular.eot">
+	<link rel="stylesheet" type="text/css" href="fonts/glyphicons-halflings-regular.svg">
+	<link rel="stylesheet" type="text/css" href="fonts/glyphicons-halflings-regular.ttf">
+	<link rel="stylesheet" type="text/css" href="fonts/glyphicons-halflings-regular.woff">
+	<link rel="stylesheet" type="text/css" href="fonts/glyphicons-halflings-regular.woff2">
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css.map">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css.map">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="css/sticky.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/checkbox.min.css">
+
+
+    <script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/sticky.js"></script>
+	<script src="js/bootstrap-select.js"></script>
+	<script src="js/checkbox.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+
+</head>
+
+<body>
+
+ <div class="container-fluid div-header-black">
+    <!-- <div class="col-md-12 "> -->
+		<form class="form-inline" role="form">
+			<div class="form-group marginleft65px floatleft">
+				<h3 id="h2white">Clasified Ads</h3>
+
+			</div>
+			<div class="form-group align-top-center-header floatright marginright65px">
+				<button class="StyleRedButton">Post Your Add</button>
+				<span class="div-padding5px">
+					<img src="img/flagbritain.png" class="flagsize">
+				</span>
+				<span class="div-padding5px">
+					<div class="ui toggle checkbox BlackCheckboxSectionUI">
+  						<input type="checkbox" name="public">
+  						<label></label>
+					</div>
+				</span>
+				<span class="div-padding5px">
+        			<img src="img/flagspain.png" class="flagsize">
+				</span>
+			</div>
+		</form>
+    <!-- </div>	 -->
+</div>
+<div class="container-fluid div-header">
+    <!-- <div class="col-md-12 "> -->
+
+		<form class="form-inline" role="form">
+			<div>
+				<div class="form-group align-top-center-header marginleft65px " >
+					<select class="selectcity">
+						<option>Los Angeles</option>
+						<option>Los Angeles</option>
+						<option>Washington</option>
+						<option>New York</option>
+					</select>
+				</div>
+	
+				<div class="form-group align-top-center-header" id="inputhead1">
+					<input type="email" class="form-control widthinput100percent" id="email" 	placeholder="What ary you looking?">
+				</div>
+				<div class="form-group align-top-center-header">
+					<div>
+						<div class="btn-group bootstrap-select show-tick"><button type="button" class="	btn dropdown-toggle btn-default" data-toggle="dropdown" title="Nothing selected	" aria-expanded="false"><span class="filter-option pull-left">Show category</	span>&nbsp;<span class="bs-caret"><span class="caret"></span></span></button>
+							<div class="dropdown-menu open" style="max-height: 496.091px; overflow: hidden;	 min-height: 87px;"	>
+								<ul class="dropdown-menu inner" role="menu" style="max-height: 486.091px; 	overflow-y: auto; 	min-height:	 77px;"><li data-original-index="0">
+								<a tabindex="0" class="" style="" data-tokens="null">
+								<span 	class="text">Category 1</span>
+								<span class="glyphicon glyphicon-ok check-mark"></span></a></li>
+								<li data-original-index="1"><a tabindex="0" class="" style="" data-tokens="	null"><span class="	text">Category 2	</span><span class="glyphicon 	glyphicon-ok check-mark"></span></a></li><li 	data-original-index="2"><a 		tabindex="0" class="" style="" data-tokens="null"><span 	class="text">	Category 3</span><span class="	glyphicon glyphicon-ok check-mark"></span></		a></li><li data-original-index="3"><a tabindex="0" class=""	 style="" 	data-tokens="null">	<span class="text">Category 4</span><span class="	glyphicon glyphicon-ok 	check-mark"></span	></a></li>
+								</ul>
+							</div>
+						</div>
+						<button type="button" class="btn btn-default btn "><span class="	SearchButtonSpan">Search</span></button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+<div class="container-fluid">
+    <div class="row marginleft40px">
+        <!-- <div class="col-fixed-240"><button>Click Me</button></div> -->
+        <div class="col-md-12 col-offset-400">
+           	<div class="row">
+        	<div class="col-md-4 div-first-row div-outer width30 minwidth150">
+        		<div class="div-white-bckg boxshadow">
+        			<div class="div-inside" id="price11" >
+        				<img src="img/honda01.jpg" class="image" >
+        				<h3><b>Bueno y ahorrador de gasolina</b></h3>
+        		  		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus 	accursus 		commodo, tortor mauris condimentum nibh, ut fermentum massa 	justosit amet risus. Etiam 		porta sem malesuada magna mollis euismod. 	Donecsed odio dui. </p>
+        		  	</div>
+        		</div>
+        		<div class="div-footer-gray div-inside boxshadow">
+        			<span>Cars » Honda</span>
+        			<p>Today, 11:11 am</p>
+        		</div>
+        	</div>
+        	<div class="col-md-4 div-first-row div-outer width30 minwidth150">
+        		<div class="div-white-bckg boxshadow">
+        			<div id="price12" class="div-inside div-white-bckg div-outer" >
+        				<h3><b>Bonita van familiar</b></h3>
+        	  			<p>Vendo caravan 2000, con 129xxx milas, motor y transicion en buen estabo, vidrios electricos.</p>
+        	  		</div>
+        	  		<div class="div-footer-gray div-inside boxshadow">
+        				<span>Cars » Honda</span>
+        				<p>Today, 11:11 am</p>
+        			</div>
+        	  	</div>
+				<div class="div-white-bckg boxshadow">
+					<div  class="div-inside  div-white-bckg div-outer margintop10">
+        				<h3 ><b>Limpio</b></h3>
+        	  			<p>Vendo dodge neon aire acondisio-nando titulo limpio 4 silindros 19.700 milas 5628465854</p>
+        	  		</div>
+        	  		<div class="div-footer-gray div-inside boxshadow">
+        				<span>Cars » Honda</span>
+        				<p>Today, 11:11 am</p>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-md-4 div-first-row div-outer width30 minwidth150">
+        		<div class="div-flesh boxshadow">
+        			<div class="div-inside">
+        				<img src="img/honda5.png" class="image" >
+        				<h3><b>Bueno y ahorrador de gasolina</b></h3>
+        	  			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus accursus 		commodo, 	tortor mauris condimentum nibh, ut fermentum massa justosit amet risus. Etiam 		porta sem malesuada 	magna mollis euismod. Donecsed odio dui. </p>
+        	  		</div>
+        	  		<div class="div-footer-flesh div-inside boxshadow">
+        				<span>Cars » Honda</span>
+        				<p>Today, 11:11 am</p>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-fixed-300">
+        		<div class="boxshadow">
+        			<div class="div-inside-banner">
+        				<img src="img/wall1.png" class="image" >
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall2.png" class="image">
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall3.png" class="image">
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall4.png" class="image">
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall5.png" class="image">
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall2.png" class="image">
+        	  		</div>
+        		</div>
+        		<div class="boxshadow margintop10">
+        			<div class="div-inside-banner">
+        				<img src="img/wall3.png" class="image">
+        	  		</div>
+        		</div>
+        	</div>
+      		</div>
+        </div>
+        
+    </div>
+
+    <div class="row marginleft40px">
+        <!-- <div class="col-fixed-240"><button>Click Me</button></div> -->
+        <div class="col-md-12 col-offset-400">
+           	<div class="row">
+        	<div class="col-md-8 div-outer width60percent minwidth150">
+        		<div class="div-white-bckg boxshadow">
+        			<div class="div-inside" id="price11" >
+        				<img src="img/honda2.jpg" class="image" >
+        				<h3><b>Golf todo original</b></h3>
+        		  		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus 	accursus 		commodo, tortor mauris condimentum nibh, ut fermentum massa 	justosit amet risus. Etiam 		porta sem malesuada magna mollis euismod. 	Donecsed odio dui. </p>
+        		  	</div>
+        		</div>
+        		<div class="div-footer-gray div-inside boxshadow">
+        			<span>Cars » Honda</span>
+        			<p>Today, 11:11 am</p>
+        		</div>
+        	</div>
+        	<div class="col-md-4 div-outer width30 minwidth150">
+        		<div class="div-flesh boxshadow">
+        			<div class="div-inside">
+        				<img src="img/honda6.jpg" class="image" >
+        				<h3><b>Bueno y ahorrador de gasolina</b></h3>
+        	  			<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus accursus 		commodo, 	tortor mauris condimentum nibh, ut fermentum massa justosit amet risus. Etiam 		porta sem malesuada 	magna mollis euismod. Donecsed odio dui. </p>
+        	  		</div>
+        	  		<div class="div-footer-flesh div-inside boxshadow">
+        				<span>Cars » Honda</span>
+        				<p>Today, 11:11 am</p>
+        			</div>
+        		</div>
+        	</div>
+      		</div>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
